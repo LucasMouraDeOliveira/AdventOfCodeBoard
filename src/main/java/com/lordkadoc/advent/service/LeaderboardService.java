f6f3fda6ea834bbdd15f1e5a66b6251ab1ae0d1a
Lucas Moura de Oliveira <lucas.mourado@gmail.com>
2020-11-23 17:59:50 +0100

    stuff

---
+++ src/main/java/com/lordkadoc/advent/service/LeaderboardService.java
@@ -0,0 +1,12 @@
+package com.lordkadoc.advent.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LeaderboardService {
+	
+	public String getLeaderboard() {
+		return "lucas: first";
+	}
+
+}
